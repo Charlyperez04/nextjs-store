56d64a0f0b93804efb68574cc1b3fbc959d50d69
Charly <charlyperezpop@gmail.com>
2024-06-22 12:19:20 -0600

    Errors

---
+++ src/app/(home)/error.tsx
@@ -0,0 +1,22 @@
+"use client"
+
+import { useEffect } from "react";
+
+interface ErrorProps{
+    error: Error;
+    reset:()=>void;
+}
+export default function Error({error,reset}: ErrorProps){
+    useEffect(()=>{
+        console.log(error)
+    })
+    return(
+        <div style={{
+            padding:'10rem'
+        }}>
+            <h1>:C</h1>
+            <p>Errorrrororororoo</p>
+            <button onClick={reset}>Intentar de nuevo</button>
+        </div>
+    )
+}
